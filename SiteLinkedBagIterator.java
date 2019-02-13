f8031fb9b04469e2e8fab251da2998b4e4c9588b
Robel Tesfaye <robel.tesfaye20@gmail.com>
2019-02-13 14:15:18 -0600

    Add files via upload

---
+++ SiteLinkedBagIterator.java
@@ -0,0 +1,30 @@
+package siteCollection;
+
+import java.util.Iterator;
+
+public class SiteLinkedBagIterator implements Iterator<SiteReading> {
+	
+	private SiteNode current;
+	public SiteLinkedBagIterator(SiteNode head){
+		this.current = head;
+	}
+
+	@Override
+	public boolean hasNext() {
+		if (this.current == null)
+			return false;
+		
+		return true;
+	}
+
+	@Override
+	public SiteReading next() {
+		
+		SiteReading data = this.current.getData();
+		
+		this.current = this.current.getLink();
+		
+		return data;
+	}
+
+}
