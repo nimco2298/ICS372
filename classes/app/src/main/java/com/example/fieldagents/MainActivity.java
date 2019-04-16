5430e1b8c1d4dcb22176f1bcba3a0ddc118ad5a2
Nimco <nimco@Nim-MacBook-Air.local>
2019-04-16 08:42:43 -0500

    reading screen compiles now

--- classes/app/src/main/java/com/example/fieldagents/MainActivity.java
+++ classes/app/src/main/java/com/example/fieldagents/MainActivity.java
@@ -9,8 +9,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private JSON jsonInfo = new JSON();
+    TextView display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +24,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //Toolbar toolbar = findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
+
+        Button buttonReadJson = findViewById(R.id.buttonReadJson);
+        buttonReadJson.setOnClickListener(this);
+
     }
 
+    //when Read JSON button is clicked, it will launch code in JSON class
+    public void onClickReadJSON (View view){
+
+        TextView screen = findViewById(R.id.textShow); //display readings
+
+        String readingsList= jsonInfo.readJSON();
+        screen.setText(readingsList);
+
+       // display.setText(readings);
+    }
+
+    //
+    /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -41,4 +64,18 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+//
+*/
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.buttonReadJson1:
+                //mini notiication on screen
+                Toast.makeText(getApplicationContext(), "Button Read JSON clicked!",
+                        Toast.LENGTH_LONG).show();
+                break;
+
+        }
+    }
+
 }
--- classes/app/src/main/java/com/example/fieldagents/ReaderWriter.java
+++ classes/app/src/main/java/com/example/fieldagents/ReaderWriter.java
@@ -3,7 +3,9 @@ package com.example.fieldagents;
 import android.content.res.AssetManager;
 import android.content.*;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 //class that acts like SiteReader for Android Studio
 public class ReaderWriter {
@@ -16,7 +18,6 @@ public class ReaderWriter {
         try {
             InputStream i = a.open(assetName);
 
-            //ERROR HERE! CANNOT FIND SYMBOL METHOD STRINGFROMSTREAM(InputStream), is i an INPUTSTREAM object?
             String chosenFile = ReaderWriter.stringFromStream(i);
             i.close();
             return chosenFile;  //obtain the file
@@ -26,5 +27,30 @@ public class ReaderWriter {
 
         }
         return null;  //return null if file cannot be found
+    }
+
+
+    /**Method will take a file, and create a new strign that has the contents of the file
+     *
+     * @param  i InputStream
+     * @return a String representation of a file
+     */
+    public static String stringFromStream(InputStream i){
+
+        try{
+            BufferedReader reader = new BufferedReader(new InputStreamReader(i));
+            StringBuilder s = new StringBuilder();
+            String currLine = null;
+
+            while((currLine = reader.readLine()) != null) {
+                s.append(currLine).append("\n");
+            }
+            reader.close();
+        }
+        catch(Exception e){
+
+        }
+
+        return null;
     }
 }
